0bf846c5ed8a8a559ca47619dbdde0190f047ba6
Bruno Rodrigues dos Reis Rossi Serra <100791305+BrunoRSerra@users.noreply.github.com>
2022-03-15 17:47:33 -0300

    ajustes final nos exercicios
    fim dos exercícios com acréscimo de uma linha

--- Aula01/src/aulas_exercicios/Exercicio5.java
+++ Aula01/src/aulas_exercicios/Exercicio5.java
@@ -10,21 +10,22 @@ public class Exercicio5 {
 		int numero = 0;
 		int resultado=0;
 		int digitado;
-	
+		int total=0;
 		
 		
 		for(int contador =1;contador>0;contador++) {
 			System.out.println("Digite um numero ");
 			digitado = teclado.nextInt();
 			resultado +=digitado;
-			
+			total++;
 			if(digitado==0) {
 				
 				break;
 			}
 			
 		}
-		System.out.println("Você digitou 0 por isso o programa parou");
+		System.out.println("Você digitou 0 e por isso o programa parou");
+		System.out.println("Você digitou "+(total-1)+" números");
 		System.out.println("A soma dos valores digitado foi de "+resultado);
 			
 		
